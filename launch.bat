c3aa3abd8dfa68d527404b20c10c26c4437f53bd
yarden.regev <excerpta.app@gmail.com>
2024-10-20 14:52:40 +0300

    removed the specification of the directory in the bat file

--- launch.bat
+++ launch.bat
@@ -1,5 +1,5 @@
 @echo off
-call C:\Users\yardi\OneDrive\Desktop\Yarden\Code\remote_pc_trackpad\env\Scripts\activate.bat
-cd C:\Users\yardi\OneDrive\Desktop\Yarden\Code\remote_pc_trackpad\pc_remote
+call env\Scripts\activate.bat
+cd pc_remote
 python manage.py runserver_plus --cert-file selftest-cert.crt 0.0.0.0:8443
 pause
