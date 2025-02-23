3d523e318b0e5bfeb2355b00a94f13bcf2cbc824
yarden.regev <excerpta.app@gmail.com>
2025-02-23 13:15:17 +0200

    for simplicity, made django debug be always false

--- pc_remote/pc_remote/settings.py
+++ pc_remote/pc_remote/settings.py
@@ -31,7 +31,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  env.bool('DJANGO_DEBUG',default=True)
+DEBUG =  False
 
 host = gethostname()
 
